91ccb6aad58f264aec00df7119f7c6612d10536b
DamienSerin <damien.serin@gmail.com>
2016-03-26 18:50:01 +0100

    init project

---
+++ ARDroneAPI/ARDroneAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARDroneAPI
+//
+//  Created by Damien Serin on 26/03/2016.
+//  Copyright © 2016 Damien Serin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
