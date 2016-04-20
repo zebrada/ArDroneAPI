15ba87fc46705070c273102130b580ae2b707814
Mickael Goualard <mickael.goualard@gmail.com>
2016-04-20 18:04:51 +0200

    Ajout du framework au projet test

--- ARDroneAPI/ARDroneAPI/StartViewController.swift
+++ ARDroneAPI/ARDroneAPI/StartViewController.swift
@@ -8,21 +8,17 @@
 
 import UIKit
 import CocoaAsyncSocket
+import ARDroneAPIFramework
 
 class StartViewController: UIViewController {
     
-    var inSocket : InSocket!
-    var outSocket : OutSocket!
-    var commandQueue: CommandQueue!
+    let drone = ARDrone()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        commandQueue = CommandQueue()
-        inSocket = InSocket()
-        outSocket = OutSocket(cmdQueue: commandQueue)
-        outSocket.send("This is a message!")
         // Do any additional setup after loading the view.
+        drone.connect()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,35 +26,4 @@ class StartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    @IBAction func fafaf(sender: AnyObject) {
-        inSocket.send("message")
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func clickDecollage(sender: UIButton) {
-        
-        commandQueue.add(TakeOffCommand())
-    }
-    
-    @IBAction func clickAtterrissage(sender: UIButton) {
-        
-        commandQueue.add(LandCommand())
-    }
-    
-    @IBAction func gaucheButtonDown(sender: AnyObject) {
-        commandQueue.add(PCMDCommand(hover: false, combined_yaw_enabled: false, left_right_tilt: 0, front_back_tilt: 0, vertical_speed: 0, angular_speed: -0.3))
-    }
-    
-    @IBAction func droiteButtonDown(sender: AnyObject) {
-        commandQueue.add(PCMDCommand(hover: false, combined_yaw_enabled: false, left_right_tilt: 0, front_back_tilt: 0, vertical_speed: 0, angular_speed: 0.3))
-    }
 }
